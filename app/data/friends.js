93b836cd297729b9b63164a48c7235834ec696de
Jan Jakubowski <jan.jakubowski.us@ieee.org>
2019-06-27 14:27:25 -0400

    questions added, friends data cleaned up

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,17 +1,25 @@
 // Array of objects holding the database of users who have completed the survey
 //
+// Max Score is 50, Min Score is 10
+// Andy is a 43
+// Betty is a 40
+// Charles is 37
+// Dora is a loser at 10
+// Eugene is a 34
+// Fran is a 30
+//
 var friends = [{
-    name: "Adam",
+    name: "Andy",
     avatar: 'https://avataaars.io/?avatarStyle=Circle&topType=LongHairShavedSides&accessoriesType=Kurt&facialHairType=MoustacheFancy&facialHairColor=Blonde&clotheType=BlazerSweater&eyeType=Surprised&eyebrowType=Angry&mouthType=Tongue&skinColor=Tanned',
-    scores: [1, 2, 3, 4, 5, 1, 2, 3, 4, 5]
+    scores: [1, 2, 5, 5, 5, 5, 5, 5, 5, 5]
 },{
     name: "Betty",
     avatar: 'https://avataaars.io/?avatarStyle=Circle&topType=LongHairStraight&accessoriesType=Kurt&hatColor=Blue01&hairColor=Blonde&facialHairType=Blank&facialHairColor=BlondeGolden&clotheType=ShirtCrewNeck&clotheColor=PastelOrange&eyeType=Close&eyebrowType=SadConcerned&mouthType=ScreamOpen&skinColor=Pale',
-    scores: [1, 3, 5, 1, 3, 5, 1, 3, 5, 1]
+    scores: [1, 2, 3, 4, 5, 5, 5, 5, 5, 5]
 },{
     name: "Charles",
     avatar: 'https://avataaars.io/?avatarStyle=Circle&topType=LongHairShavedSides&accessoriesType=Kurt&facialHairType=MoustacheFancy&facialHairColor=Blonde&clotheType=BlazerSweater&eyeType=Surprised&eyebrowType=Angry&mouthType=Tongue&skinColor=Tanned',
-    scores: [2, 4, 2, 4, 2, 4, 2, 4, 2, 4]
+    scores: [1, 1, 2, 3, 5, 5, 5, 5, 5, 5]
 },{
     name: "Dora",
     avatar: 'https://avataaars.io/?avatarStyle=Circle&topType=LongHairBigHair&accessoriesType=Wayfarers&hairColor=Red&facialHairType=Blank&facialHairColor=Platinum&clotheType=GraphicShirt&clotheColor=Gray01&graphicType=Cumbia&eyeType=Hearts&eyebrowType=Angry&mouthType=Default&skinColor=Pale',
@@ -19,11 +27,11 @@ var friends = [{
 },{
     name: "Eugene",
     avatar: 'https://avataaars.io/?avatarStyle=Circle&topType=Eyepatch&accessoriesType=Round&hairColor=BlondeGolden&facialHairType=BeardMagestic&facialHairColor=Platinum&clotheType=Overall&clotheColor=Blue01&eyeType=Default&eyebrowType=UnibrowNatural&mouthType=Default&skinColor=Pale',
-    scores: [5, 4, 3, 5, 4, 3, 5, 4, 3, 5]
+    scores: [1, 1, 2, 2, 4, 4, 5, 5, 5, 5]
 },{
     name: "Frances",
     avatar: 'https://avataaars.io/?avatarStyle=Circle&topType=LongHairFro&accessoriesType=Prescription01&hairColor=Red&facialHairType=Blank&clotheType=ShirtVNeck&clotheColor=Blue03&eyeType=Close&eyebrowType=UpDown&mouthType=Default&skinColor=DarkBrown',
-    scores: [5, 2, 3, 5, 2, 3, 5, 2, 3, 4]
+    scores: [1, 1, 2, 2, 3, 3, 4, 4, 5, 5]
 }
 ];
 
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -20,55 +20,51 @@ module.exports = function(app) {
 
 	app.post("/api/friends", function(req, res) {
 
-		console.log("post req: " + JSON.stringify(req.body));
-			
-			var user = req.body;
-			var scoresString = user.scores;
+		var user = req.body;
+		var scoresString = user.scores;
 
-			// convert scores to array of integers to do math
-			// var scores = scoresString.map(function(score) {
-			// 	return parseInt(score);
-			// });
-			var scores = [];
-			for (var j=0; j < scoresString.length; j ++) {
-				scores[j] = parseInt(scoresString);
-			};
+		// convert scores to array of integers to do math
+		// var scores = scoresString.map(function(score) {
+		// 	return parseInt(score);
+		// });
+		var scores = [];
+		for (var j=0; j < scoresString.length; j ++) {
+			scores[j] = parseInt(scoresString);
+		};
 
-			newUser = {
-				name: user.name,
-				avatar: user.avatar,
-				scores: scores
-			};
+		newUser = {
+			name: user.name,
+			avatar: user.avatar,
+			scores: scores
+		};
 
-			var totalScore = 0;
-				for (var j=0; j < scores.length; j ++) {
-						totalScore += scores[j];
-					};
-
-			// default best match to first one in friendsDB
-			var bestMatchIndex = 0;
-			var bestMatchDifference = 40;       // diff of 10 - 5's and 10 - 1's is 40
-
-			for(var i = 0; i < friends.length; i++) {
-				     
-				nextScores = friends[i].scores;
-				
-				var nextTotalScore = 0;
-				for (var j=0; j < nextScores.length; j ++) {
-						nextTotalScore += nextScores[j];
-					};
-				var totalDifference = Math.abs(totalScore - nextTotalScore);
-
-				// if there is a new best match, point new best match's index and set the new best diff
-				if(totalDifference < bestMatchDifference) {
-					bestMatchIndex = i;
-					bestMatchDifference = totalDifference;
+		var totalScore = 0;
+			for (var j=0; j < scores.length; j ++) {
+					totalScore += scores[j];
 				};
+
+		// default best match to first one in friendsDB
+		var bestMatchIndex = 0;
+		var bestMatchDifference = 40;       // diff of 10 - 5's and 10 - 1's is 40
+
+		for(var i = 0; i < friends.length; i++) {
+					
+			nextScores = friends[i].scores;
+			
+			var nextTotalScore = 0;
+			for (var j=0; j < nextScores.length; j ++) {
+					nextTotalScore += nextScores[j];
+				};
+			var totalDifference = Math.abs(totalScore - nextTotalScore);
+
+			// if there is a new best match, point new best match's index and set the new best diff
+			if(totalDifference < bestMatchDifference) {
+				bestMatchIndex = i;
+				bestMatchDifference = totalDifference;
 			};
+		};
 
-			friends.push(newUser);
+		friends.push(newUser);
 
-			res.json(friends[bestMatchIndex]);
+		res.json(friends[bestMatchIndex]);
 	});
-
-};
